3292fdedd2e890f84e5c0fd919dcc86dd3037991
iam-i-j-k <irfanjankhan7860@gmail.com>
2024-11-17 00:53:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './index.css'
+import Canvas from './Canvas'
+import data from './data'
+import LocomotiveScroll from 'locomotive-scroll';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const App = () => {
+
+  
+  const [showCanvas, setShowCanvas] = useState(false);
+  const headingRef = useRef(null);
+  const growingSpan = useRef(null);
+  useEffect(()=>{
+    const locomotiveScroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+      smoothMobile: true
+    });
+  },[]);
+
+  useGSAP(()=>{
+    document.addEventListener('mousemove', (e) => {
+      gsap.to(growingSpan.current, {
+        top: e.clientY,
+        left: e.clientX,
+        duration: 0.6,
+      });
+    });
+
+    headingRef.current.addEventListener('click',(e)=>{
+      setShowCanvas(!showCanvas);
+      gsap.set(growingSpan.current,{
+        top: e.clientY,
+        left: e.clientX,
+        scale: showCanvas ? 120 : 0,
+      });
+
+      gsap.to("body", {
+        color: showCanvas ? "#fff" : "#000",
+        duration: 0.5,
+        ease: "power2.inOut",
+      });
+
+      gsap.to(growingSpan.current,{
+        scale: showCanvas ? 0 : 200,
+        duration: 1.25,
+        ease: "power2.inOut",
+      });
+
+      
+    });
+  },[showCanvas]);
+  return (
+    <>
+      <span ref={growingSpan} className="growing block top-[-20px] left-[-20px] w-4 h-4 sm:w-5 sm:h-5 fixed rounded-full"></span>
+      <div data-scroll-container>
+        
+        <div className="w-full absolute h-screen text-white text-5xl">
+      {showCanvas && data[0].map((item,index)=>(
+            <Canvas key={index} details={item} />
+          ))}
+      
+
+        </div>
+        <div className='w-full relative h-screen'>
+          <nav className="w-full p-2 sm:p-3 border-b border-[#00000080] z-50 tracking-tighter">
+            <div className="flex flex-col sm:flex-row justify-between items-center">
+              <div className="text-sm sm:text-md font-light tracking-tight">Thirtysixstudio</div>
+              <ul className="flex gap-4 sm:gap-8 text-sm sm:text-base mt-2 sm:mt-0">
+                {["Home", "About", "Projects", "Contact"].map((link, index) => (
+                  <li key={index} className="hover:text-yellow-500 cursor-pointer transition-colors">
+                    <a data-scroll-to={`#${link.toLowerCase()}`} href={`#${link.toLowerCase()}`}>{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
+          <div id="home" className='textcontainer w-full p-38 md:px-[25%] md:py-[45px]'>
+            <div className='text w-full px-1 py-20 md:w-[45%] md:p-0'>
+
+              <h3 className='text-3xl tracking-tight leading-[1.2]'>
+                At Thirtysixstudio, we build immersive digital experiences for brands with a purpose.
+              </h3>
+              <p className='text-sm mt-5 font-lighter md:mt-10 md:w-[80%]'>
+                We're a boutique production studio focused on design, motion, and cr
+                eative technology, constantly reimagining what digital craft can do for present-time ads and campaigns.
+              </p>
+              <p className='text-md mt-10'>Scroll</p>
+            </div>
+          </div>
+          
+          <div className="w-full">
+          
+            <h1
+            ref={headingRef}
+            className="text-5xl sm:text-7xl md:text-9xl flex flex-wrap md:flex-nowrap tracking-tight px-2">
+              <span className='w-full md:w-auto md:text-[12.33rem] md:pl-3'>Thirty</span><span className='w-full md:w-auto md:text-[12.33rem]'>six</span><span className='w-full md:w-auto md:text-[12.33rem]'>studio</span>
+            </h1>
+          </div>
+          <div className="w-full absolute h-screen text-white text-5xl">
+      
+      {showCanvas && data[1].map((item,index)=>(
+            <Canvas key={index}  details={item} />
+          ))}
+
+          </div>
+          <div id="about" data-scroll-section className='w-full min-h-screen mt-16 sm:mt-32 p-4 sm:p-10'>
+            <h2 className='text-3xl sm:text-5xl font-semibold'>
+            OUR SERVICES
+            </h2>
+            <p className='text-lg sm:text-2xl leading-[1.8] sm:leading-[2] tracking-tight mt-4 sm:mt-5 mb-6 sm:mb-10 font-[300] md:mt-10 md:w-[80%] '>
+              We're a boutique production studio focused on design, motion, and cr
+              eative technology, constantly reimagining what digital craft can do for present-time ads and campaigns.
+              <br />
+              We provide captivating design, interactive animations, advancedusability, reliablecode, and immaculate project coordination. Whether you need a campaign built from scratch or assistance at a specific phase, we’ve got you covered.
+            </p>
+            
+            <img 
+              src="https://api.funkhaus.us/wp-content/uploads/2024/08/Hero-Image-1-808x540.jpg" 
+              alt="" 
+              className="w-full h-auto object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Canvas.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react"
+import canvasImages from "./canvasImages";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Canvas = ({details}) => {
+    // console.log(startIndex);
+    const { duration, numImages, startIndex, size, top, left, zIndex } = details;
+    const [index, setIndex] = useState({value: startIndex});
+    const canvasRef = useRef(null);
+    useGSAP(()=>{
+        gsap.to(index, {
+            value: startIndex + numImages - 1,
+            duration: duration,
+            repeat: -1,
+            ease: "linear",
+            onUpdate: () =>{
+                setIndex({value: Math.round(index.value)});
+            }
+        });
+
+        gsap.from(canvasRef.current,{
+            opacity: 0,
+            duration: 0.5,
+            ease: "power2.inOut"
+        });
+    });
+
+    useEffect(() => {
+      const scale = window.devicePixelRatio;
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      const img = new Image();
+      img.src = canvasImages[index.value];
+      img.onload = ()=>{
+        canvas.width = canvas.offsetWidth * scale;
+        canvas.height = canvas.offsetHeight * scale;
+        canvas.style.width = canvas.offsetWidth + "px";
+        canvas.style.height = canvas.offsetHeight + "px";
+        ctx.drawImage(img,0,0, canvas.offsetWidth, canvas.offsetHeight);
+        ctx.scale(scale, scale);
+      };
+    }, [index]);
+    
+  return <canvas
+   ref={canvasRef}
+   data-scroll
+   data-scroll-speed={
+   Math.random().toFixed(2)
+   }
+    className="absolute" 
+    style={{width: `${size*1.3}px`, height: `${size*1.3}px`, top: `${top}%`, left: `${left}%`, zIndex: zIndex}} 
+    id="canvas">
+
+    </canvas>
+}
+
+export default Canvas
